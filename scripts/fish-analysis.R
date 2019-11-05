fd9ca2cb9d642a0be755e1644c1569fb66ce1b18
allentpe <allentpe@uw.edu>
2019-11-05 10:26:05 -0800

    added a new column length_cat

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -2,5 +2,8 @@
 fish_data = read.csv("data/Gaeta_etal_CLC_data.csv")
 
 library(dplyr)
+
+# this code adds a column called length_cat, and if length is greater than 300, big is added in column, if not small is
+
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
