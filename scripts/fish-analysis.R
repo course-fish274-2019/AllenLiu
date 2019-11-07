cf8f711edc2445c8253e7c3036eb9f1e43303e15
allentpe <allentpe@uw.edu>
2019-11-07 10:06:31 -0800

    added more comments in the script to make it more descriptive.

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -6,19 +6,20 @@ fish_data = read.csv("data/Gaeta_etal_CLC_data_1.csv")
 library(dplyr)
 
 # this code adds a column called length_cat, and if length is greater than 300, big is added in column, if not small is
-
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 
+#Filter out scalelengths less than 1
 fish_data_cat_clean <- filter(fish_data_cat, scalelength > 1)
 
 #Question 5 - Graph 
 library(ggplot2)
 
+#The color = lakeid argument is within the aes() because it is related to the data, not just a change in color aesthetically
 ggplot(fish_data_cat_clean, aes(x = length, y = scalelength, color = lakeid)) +
   geom_point(size = 3)
 
-
+#Bins changes how wide the columns are in the histogram 
 ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
   geom_histogram(bins=80)
 
